5635e9027ab8d489a15300093cb00c5eeedfbe5c
fenghe <jxphxufh@gmail.com>
2019-11-03 19:03:00 +0800

    add socket_server.py

--- auto_depoly_docker/ssh_login.py
+++ auto_depoly_docker/ssh_login.py
@@ -1,5 +1,12 @@
 import paramiko
 
+
+def execute_command(client, command):
+    stdin, stdout, stderr = client.exec_command(command)
+    output = stdout.readlines()
+    print(output)
+
+
 host = '45.146.122.60'
 port = 22122
 username = 'root'
@@ -8,7 +15,9 @@ password = 'md02AZX3gMB61m2oWe'
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.connect(host, port=port, username=username, password=password)
-stdin, stdout, stderr = client.exec_command("pwd")
-output = stdout.readlines()
-print(output)
+execute_command(client, "git clone https://github.com/Bill0412/blogdiy.git")
+execute_command(client, "cd blogdiy/auto_deploy_docker")
+execute_command(client, "nohup python ")
+
+
 client.close()
