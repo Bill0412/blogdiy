81e84a6173613e18ffe28d86db158db482195037
fenghe <jxphxufh@gmail.com>
2019-11-04 04:42:56 +0800

    feat: wss

--- auto_deploy_docker/web_socket.py
+++ auto_deploy_docker/web_socket.py
@@ -36,6 +36,6 @@ ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
 ssl_context.load_cert_chain(localhost_pem)
 
-start_server = websockets.serve(deploy_service, '0.0.0.0', 6789, ssl=ssl_context)
+start_server = websockets.serve(deploy_service, 'deploy.blogdiy.net', 6789, ssl=ssl_context)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
