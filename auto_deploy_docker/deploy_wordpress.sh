965fd9bb064c380376fe3877825cc721e4ada9d8
fenghe <jxphxufh@gmail.com>
2019-11-04 22:19:48 +0800

    add deploy_wordpress.sh

--- auto_deploy_docker/deploy_wordpress.sh
+++ auto_deploy_docker/deploy_wordpress.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+apt install docker.io && echo "Docker is installed"
+echo ""
+
+docker pull wordpress && echo "wordpress image is ready"
+echo ""
+
+docker run --name wp0 80:80 -d wordpress || { echo "Unable to deploy at port 80"; exit 1; }
+
+echo "Docker running on port 80"
