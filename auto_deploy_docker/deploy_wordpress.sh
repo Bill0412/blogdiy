908204eeb9d0ca4301d3aff540b04d8eaf1fb585
fenghe <jxphxufh@gmail.com>
2019-11-04 23:37:58 +0800

    deploy wordpress on any port

--- auto_deploy_docker/deploy_wordpress.sh
+++ auto_deploy_docker/deploy_wordpress.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+LISTEN_PORT=$1
 
 echo "Installing docker..."
 echo 'y\n' | apt install docker.io
@@ -11,7 +12,7 @@ echo "Wordpress Docker image is ready."
 echo ""
 
 echo "Trying to run docker on port 80..."
-docker run --name wp0 -p 80:80 -d wordpress || { echo "Unable to deploy at port 80"; exit 1; }
-echo "Woredpress container listening on port 80."
+docker run --name wp0 -p ${LISTEN_PORT}:80 -d wordpress || { echo "Unable to deploy at port "${LISTEN_PORT}; exit 1; }
+echo "Woredpress container listening on port "${LISTEN_PORT}"."
 
 echo "Setup success!!!"
