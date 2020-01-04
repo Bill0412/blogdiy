117081b348659fcd5d98f14f86be5b8b1ec9b5a2
fenghe <jxphxufh@gmail.com>
2020-01-05 06:36:37 +0800

    init docker-compose.yml

--- auto_deploy_docker/deploy_wordpress.sh
+++ auto_deploy_docker/deploy_wordpress.sh
@@ -6,6 +6,7 @@ echo 'y\n' | apt install docker.io
 echo "Docker is installed."
 echo ""
 
+## TODO: modify this to wordpress+mysql
 echo "Downloading Wordpress Docker image..."
 docker pull wordpress
 echo "Wordpress Docker image is ready."
@@ -14,5 +15,7 @@ echo ""
 echo "Trying to run docker on port "${LISTEN_PORT}"..."
 docker run --name "wp-port-"${LISTEN_PORT} -p ${LISTEN_PORT}:80 -d wordpress || { echo "Unable to deploy at port "${LISTEN_PORT}; exit 1; }
 echo "Woredpress container listening on port "${LISTEN_PORT}"."
+
+## TODO: setup https
 
 echo "Setup success!!!"
--- wordpress/README.md
+++ wordpress/README.md
@@ -1,1 +1,4 @@
 This is the wordpress docker with mysql composer.
+
+## References
+1. This part is based on <https://docs.docker.com/compose/wordpress/>
