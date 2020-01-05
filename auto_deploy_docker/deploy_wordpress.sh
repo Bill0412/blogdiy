cf4207c80452f5a3b9c1c0cc23211c423f15ba25
fenghe <jxphxufh@gmail.com>
2020-01-05 22:17:32 +0800

    deploy wordpress bash update

--- auto_deploy_docker/deploy_wordpress.sh
+++ auto_deploy_docker/deploy_wordpress.sh
@@ -7,13 +7,15 @@ echo "Docker is installed."
 echo ""
 
 ## TODO: modify this to wordpress+mysql
-echo "Downloading Wordpress Docker image..."
-docker pull wordpress
-echo "Wordpress Docker image is ready."
+echo "Downloading docker-compose.yml"
+wget "https://raw.githubusercontent.com/Bill0412/sitediy/wordpress/wordpress/docker-compose.yml"
+echo "The configuration file is ready."
 echo ""
 
 echo "Trying to run docker on port "${LISTEN_PORT}"..."
-docker run --name "wp-port-"${LISTEN_PORT} -p ${LISTEN_PORT}:80 -d wordpress || { echo "Unable to deploy at port "${LISTEN_PORT}; exit 1; }
+# docker run --name "wp-port-"${LISTEN_PORT} -p ${LISTEN_PORT}:80 -d wordpress || { echo "Unable to deploy at port "${LISTEN_PORT}; exit 1; }
+docker swarm init
+docker stack deploy -c docker-compose.yml wp-${LISTEN_PORT}
 echo "Woredpress container listening on port "${LISTEN_PORT}"."
 
 ## TODO: setup https
