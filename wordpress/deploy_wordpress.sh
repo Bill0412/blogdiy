d208c4ec1b6ccd17ceca7a0c6d78ab0719e96df2
fenghe <jxphxufh@gmail.com>
2020-01-18 14:18:58 +0800

    refactor wordpress

--- wordpress/deploy_wordpress.sh
+++ wordpress/deploy_wordpress.sh
@@ -14,7 +14,6 @@ echo ""
 
 echo "Trying to run docker on port "${LISTEN_PORT}"..."
 # docker run --name "wp-port-"${LISTEN_PORT} -p ${LISTEN_PORT}:80 -d wordpress || { echo "Unable to deploy at port "${LISTEN_PORT}; exit 1; }
-docker swarm init
 docker stack deploy -c stack.yml wp-${LISTEN_PORT}
 echo "Worepress container listening on port "${LISTEN_PORT}"."
 
